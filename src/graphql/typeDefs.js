19698530ff9af087968a8db13032342e979c69a3
Dan Berger <dan.berger@protonmail.com>
2020-04-05 16:36:59 -0400

    First commit

---
+++ src/graphql/typeDefs.js
@@ -0,0 +1,115 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_express_1 = require("apollo-server-express");
+exports.typeDefs = apollo_server_express_1.gql `
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+
+  input CreateBookingInput {
+    id: ID!
+    source: String!
+    checkIn: String!
+    checkOut: String!
+  }
+
+  input HostListingInput {
+    title: String!
+    description: String!
+    image: String!
+    type: ListingType!
+    address: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    country: String!
+    admin: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  enum ListingsFilter {
+    PRICE_LOW_TO_HIGH
+    PRICE_HIGH_TO_LOW
+  }
+
+  type Listings {
+    region: String
+    total: Int!
+    result: [Listing!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean
+    didRequest: Boolean!
+  }
+
+  input ConnectStripeInput {
+    code: String!
+  }
+
+  input LogInInput {
+    code: String!
+  }
+
+  type Query {
+    authUrl: String!
+    user(id: ID!): User!
+    listing(id: ID!): Listing!
+    listings(
+      location: String
+      filter: ListingsFilter!
+      limit: Int!
+      page: Int!
+    ): Listings!
+  }
+
+  type Mutation {
+    connectStripe(input: ConnectStripeInput!): Viewer!
+    createBooking(input: CreateBookingInput!): Booking!
+    disconnectStripe: Viewer!
+    hostListing(input: HostListingInput!): Listing!
+    logIn(input: LogInInput): Viewer!
+    logOut: Viewer!
+  }
+`;
